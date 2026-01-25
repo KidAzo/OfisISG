d1281e499f3ded1b0a5edcabfbf72b509a4afb1c
Azizhan Çil <azizhancil23@gmail.com>
2026-01-25 10:45:33 +0300

    fixed

--- Assets/Project/_scripts/Ray/InteractableController.cs
+++ Assets/Project/_scripts/Ray/InteractableController.cs
@@ -7,11 +7,11 @@ public class InteractableController : MonoBehaviour
 {
     [Inject] IPlayerService playerService;
     [SerializeField] LayerMask interactableLayerMask;
-    RayInteractor<IInteractable> rayInteractor;
+    RayInteractor<IRayTarget> rayInteractor;
 
     void Start()
     {
-        rayInteractor = new RayInteractor<IInteractable>(new ScreenCenterRayProvider(playerService.playerCamera), 
+        rayInteractor = new RayInteractor<IRayTarget>(new ScreenCenterRayProvider(playerService.playerCamera), 
         new PhysicsRaycastService(), 
         new RaySelector());
     }
@@ -20,9 +20,12 @@ public class InteractableController : MonoBehaviour
     {
          if (Mouse.current.leftButton.wasPressedThisFrame)
          {
-              if (rayInteractor.TryGetTarget(10f, interactableLayerMask, out RaycastHit hit, out IInteractable interactable))
+              if (rayInteractor.TryGetTarget(10f, interactableLayerMask, out IRayTarget target))
               {
-                    interactable.Interact();
+                    if (target is IInteractable interactable)
+                    {
+                        interactable.Interact();
+                    }
               }
          }
     }
