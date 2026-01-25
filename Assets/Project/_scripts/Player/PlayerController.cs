2c8fa16c53cf11abc785678788080292b264eb83
Azizhan Çil <azizhancil23@gmail.com>
2026-01-25 12:31:07 +0300

    fixed

--- Assets/Project/_scripts/Player/PlayerController.cs
+++ Assets/Project/_scripts/Player/PlayerController.cs
@@ -1,4 +1,5 @@
 using System;
+using Cysharp.Threading.Tasks;
 using Obvious.Soap;
 using Reflex.Attributes;
 using UnityEngine;
@@ -18,7 +19,7 @@ namespace Woi.Player
         [SerializeField] private float _sprintSpeed = 8f;
         [SerializeField] private float _gravity = -9.81f;
         [SerializeField] private float _groundCheckDistance = 0.2f;
-        
+
         [Header("Look Settings")]
         [SerializeField] private Transform _cameraPivotTransform;
         public Camera playerCamera;
@@ -29,101 +30,111 @@ namespace Woi.Player
         [SerializeField] private ScriptableEventVector2 moveInputEvent;
         [SerializeField] private ScriptableEventVector2 lookInputEvent;
         [SerializeField] private ScriptableEventBool sprintInputEvent;
-        
+
         private CharacterController _characterController;
         private PlayerInputActions _playerActions;
-        
+
         private Vector2 _moveInput;
         private Vector2 _lookInput;
         private bool _isSprinting;
-        
+
         private Vector3 _velocity;
         private float _cameraPitch;
-        
+        private bool _lookArmed;
+
         private void Awake()
         {
             _playerService.RegisterPlayer(this);
-            
+
             _characterController = GetComponent<CharacterController>();
             _playerActions = new();
-            
-            // Lock cursor
+        }
+
+        private void Start()
+        {
+            DelayedCursorLock().Forget();
+        }
+
+        private async UniTaskVoid DelayedCursorLock()
+        {
+            _lookInput = Vector2.zero;
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
+            
+            await UniTask.Delay(TimeSpan.FromSeconds(1f), cancellationToken: destroyCancellationToken);
+            _lookArmed = true;
         }
 
         private void OnEnable()
         {
             moveInputEvent.OnRaised += OnMove;
-            lookInputEvent.OnRaised += OnLook;  
+            lookInputEvent.OnRaised += OnLook;
             sprintInputEvent.OnRaised += OnSprint;
         }
-        
+
         private void OnDisable()
         {
             moveInputEvent.OnRaised -= OnMove;
             lookInputEvent.OnRaised -= OnLook;
             sprintInputEvent.OnRaised -= OnSprint;
         }
-        
+
         private void Update()
         {
             HandleMovement();
             HandleLook();
             ApplyGravity();
         }
-        
+
         private void HandleMovement()
         {
             float currentSpeed = _isSprinting ? _sprintSpeed : _walkSpeed;
-            
+
             Vector3 move = transform.right * _moveInput.x + transform.forward * _moveInput.y;
             _characterController.Move(move * currentSpeed * Time.deltaTime);
         }
-        
+
         private void HandleLook()
         {
-            // Horizontal rotation (Y axis - rotate player)
-            float yaw = _lookInput.x * _mouseSensitivity * Time.deltaTime;;
+            if(!_lookArmed) return;
+
+            float yaw = _lookInput.x * _mouseSensitivity;
             transform.Rotate(Vector3.up * yaw);
-            
-            // Vertical rotation (X axis - rotate camera)
-            _cameraPitch -= _lookInput.y * _mouseSensitivity * Time.deltaTime;;
+
+            _cameraPitch -= _lookInput.y * _mouseSensitivity;
             _cameraPitch = Mathf.Clamp(_cameraPitch, -_maxLookAngle, _maxLookAngle);
-            
-            if (_cameraPivotTransform != null)
-            {
-                _cameraPivotTransform.localRotation = Quaternion.Euler(_cameraPitch, 0f, 0f);
-            }
+
+            _cameraPivotTransform.localRotation = Quaternion.Euler(_cameraPitch, 0f, 0f);
         }
-        
+
         private void ApplyGravity()
         {
             if (IsGrounded() && _velocity.y < 0)
             {
                 _velocity.y = -2f; // Small downward force to keep grounded
             }
-            
+
             _velocity.y += _gravity * Time.deltaTime;
             _characterController.Move(_velocity * Time.deltaTime);
         }
-        
+
         private bool IsGrounded()
         {
             return Physics.Raycast(transform.position, Vector3.down, _groundCheckDistance + 0.1f);
         }
-        
+
         // Input callbacks
         private void OnMove(Vector2 context)
         {
             _moveInput = context;
         }
-        
+
         private void OnLook(Vector2 context)
         {
             _lookInput = context;
+            Debug.Log($"LOOK: {_lookInput}");
         }
-        
+
         private void OnSprint(bool context)
         {
             _isSprinting = context;
@@ -132,7 +143,7 @@ namespace Woi.Player
 
     public interface IPlayerService
     {
-        Camera playerCamera { get; }    
+        Camera playerCamera { get; }
         Transform GetPlayerTransform();
         void SetPlayerLocomotion(Vector3 position);
         void RegisterPlayer(PlayerController player);
@@ -156,10 +167,10 @@ namespace Woi.Player
 
         public void RegisterPlayer(PlayerController player)
         {
-            _playerController = player; 
+            _playerController = player;
             _playerTransform = player.transform;
             ch = player.GetComponent<CharacterController>();
-            OnPlayerRegistered?.Invoke();   
+            OnPlayerRegistered?.Invoke();
         }
 
         public void SetPlayerLocomotion(Vector3 position)
