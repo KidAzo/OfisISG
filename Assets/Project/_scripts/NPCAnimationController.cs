b4c03a34b24ef1fa960c798e36614f94709a5320
Azizhan Çil <105786251+KidAzo@users.noreply.github.com>
2026-02-05 22:25:14 +0300

    fixed

--- Assets/Project/_scripts/NPCAnimationController.cs
+++ Assets/Project/_scripts/NPCAnimationController.cs
@@ -8,18 +8,28 @@ public class NPCAnimationController : MonoBehaviour
     [SerializeField] private string stateName;   // Inspector’dan girilecek
     [SerializeField] private int layer = 0;
     [SerializeField] private float fadeTime = 0.1f;
-
+    [SerializeField] bool playOnStart = true;
     private int stateHash;
 
     void Start()
     {
         animator = GetComponent<Animator>();
-        stateHash = Animator.StringToHash(stateName);
-        SetAnimationState(stateHash);
+
+        if (playOnStart)
+        {
+            stateHash = Animator.StringToHash(stateName);
+            SetAnimationState(stateHash);
+        }
     }
 
     void SetAnimationState(int hash)
     {
-        animator.CrossFade(hash, fadeTime, layer);     
+        animator.CrossFade(hash, fadeTime, layer);
+    }
+
+    public void ChangeState(string newStateName)
+    {
+        int newStateHash = Animator.StringToHash(newStateName);
+        SetAnimationState(newStateHash);
     }
 }
