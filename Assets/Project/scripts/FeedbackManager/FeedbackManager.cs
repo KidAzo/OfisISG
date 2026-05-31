f7b6e168f35131f6ddb051e10b037a15478d3c16
Azizhan Çil <105786251+KidAzo@users.noreply.github.com>
2026-05-31 16:15:12 +0300

    feedback

--- Assets/Project/scripts/FeedbackManager/FeedbackManager.cs
+++ Assets/Project/scripts/FeedbackManager/FeedbackManager.cs
@@ -1,16 +1,54 @@
 using UnityEngine;
+using WoiUtils.AudioSystem;
+using WOI.Modules.SDK;
 
 public class FeedbackManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private SoundDefinition collectSound;
+
+    private AudioSystem audioSystem;
+
+    private void Awake()
     {
-        
+        if (!ServiceLocator.IsRegistered<FeedbackManager>())
+            ServiceLocator.Register(this);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        if (AudioSystem.TryGetFromServiceLocator(out audioSystem) && audioSystem != null)
+            return;
+
+        audioSystem = FindFirstObjectByType<AudioSystem>();
+        if (audioSystem == null)
+            Debug.LogWarning("[FeedbackManager] AudioSystem not found on ServiceLocator or in loaded scenes.", this);
+    }
+
+    private void OnDestroy()
+    {
+        if (ServiceLocator.TryGet(out FeedbackManager registered) && ReferenceEquals(registered, this))
+            ServiceLocator.Unregister<FeedbackManager>();
+    }
+
+    public void PlayFeedback(Transform source)
+    {
+        PlaySound(source);
+    }
+
+    public void PlaySound(Transform source = null)
+    {
+        if (collectSound == null)
+        {
+            Debug.LogWarning("[FeedbackManager] Collect sound is not assigned.", this);
+            return;
+        }
+
+        if (audioSystem == null)
+            return;
+
+        if (source != null)
+            audioSystem.Play(collectSound, PlayContext.At(source.position));
+        else
+            audioSystem.Play(collectSound);
     }
 }
--- Assets/Project/scripts/WasteCollectionMode/WasteController.cs
+++ Assets/Project/scripts/WasteCollectionMode/WasteController.cs
@@ -1,40 +1,70 @@
+using PrimeTween;
 using UnityEngine;
-using Woi.Events;
 using Woi.SelectionSystem;
+using WOI.Modules.SDK;
 
 namespace Woi.WasteCollectionMode
 {
     public class WasteController : MonoBehaviour, ISelectable
     {
-            Outline outline;
+        [SerializeField] private float scaleDuration = 0.4f;
 
-            private void Awake()
-            {
-                outline = GetComponent<Outline>();
-            }
+        private Outline outline;
+        private Vector3 initialLocalScale;
+        private Tween scaleTween;
+        private FeedbackManager feedbackManager;
 
-            private void Start()
-            {
+        private void Awake()
+        {
+            outline = GetComponent<Outline>();
+            initialLocalScale = transform.localScale;
+        }
+
+        private void Start()
+        {
+            if (ServiceLocator.TryGet(out FeedbackManager feedbackManager)) 
+                this.feedbackManager = feedbackManager;
+            else    
+                Debug.LogError("FeedbackManager not found");
+
+            if (outline != null)
                 outline.enabled = false;
-            }
+        }
 
-            private void SetOutline()
-            {
-                outline.enabled = true;
-                outline.OutlineWidth = 5f;
-            }
+        private void SetOutline()
+        {
+            if (outline == null)
+                return;
 
-            public void Select()
-            {
-                SetOutline();
+            outline.enabled = true;
+            outline.OutlineWidth = 5f;
+        }
 
-                WasteCollectable collectable = GetComponent<WasteCollectable>();
-                if (collectable != null && WasteCollectTracker.TryGetActive(out WasteCollectTracker tracker))
-                    tracker.Collect(collectable);
-            }
+        public void Select()
+        {
+            Feedback();
 
-            public void Deselect()
-            {
-            }
+            WasteCollectable collectable = GetComponent<WasteCollectable>();
+            if (collectable != null && WasteCollectTracker.TryGetActive(out WasteCollectTracker tracker))
+                tracker.Collect(collectable);
+        }
+
+        public void Deselect()
+        {
+        }
+
+        private void Feedback()
+        {
+            SetOutline();
+
+            feedbackManager.PlayFeedback(transform);
+
+            if (scaleTween.isAlive)
+                scaleTween.Stop();
+
+            transform.localScale = initialLocalScale;
+            scaleTween = Tween.Scale(transform, Vector3.zero, scaleDuration, Ease.InBack)
+                .OnComplete(gameObject, go => go.SetActive(false));
+        }
     }
 }
